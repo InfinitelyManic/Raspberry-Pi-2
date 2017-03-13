db2131dec59d0a8dd34fd3e7ba99397d0db64cc7
David <agentdaffaires@gmail.com>
2017-03-13 11:49:56 -0700

    Create rpi_2_b_header.h

---
+++ rpi_2_b_header.h
@@ -0,0 +1,133 @@
+/*
+        David @InfinitelyManic
+        Raspberry Pi 2 Model B Rev 1.1
+        Last Revision Date: 03/13/2017
+*/
+        .equ    GPFSEL0,0x0
+        .equ    GPFSEL1,0x4
+        .equ    GPFSEL2,0x8
+        .equ    GPFSEL3,0xc
+        .equ    GPFSEL4,0x10
+        .equ    GPFSEL5,0x14
+
+        .equ    FSEL53,9
+        .equ    FSEL52,6
+        .equ    FSEL51,3
+        .equ    FSEL50,0
+
+        .equ    FSEL49,27
+        .equ    FSEL48,24
+        .equ    FSEL47,21
+        .equ    FSEL46,18
+        .equ    FSEL45,15
+        .equ    FSEL44,12
+        .equ    FSEL43,9
+        .equ    FSEL42,6
+        .equ    FSEL41,3
+        .equ    FSEL40,0
+
+        .equ    FSEL39,27
+        .equ    FSEL38,24
+        .equ    FSEL37,21
+        .equ    FSEL36,18
+        .equ    FSEL35,15
+        .equ    FSEL34,12
+        .equ    FSEL33,9
+        .equ    FSEL31,3
+        .equ    FSEL30,0
+
+        .equ    FSEL29,27
+        .equ    FSEL28,24
+        .equ    FSEL27,21
+        .equ    FSEL26,18
+        .equ    FSEL25,15
+        .equ    FSEL24,12
+        .equ    FSEL23,9
+        .equ    FSEL21,3
+        .equ    FSEL20,0
+       .equ    FSEL19,27
+        .equ    FSEL18,24
+        .equ    FSEL17,21
+        .equ    FSEL16,18
+        .equ    FSEL15,15
+        .equ    FSEL14,12
+        .equ    FSEL13,9
+        .equ    FSEL12,6
+        .equ    FSEL11,3
+        .equ    FSEL10,0
+
+        .equ    FSEL9,27
+        .equ    FSEL8,24
+        .equ    FSEL7,21
+        .equ    FSEL6,18
+        .equ    FSEL5,15
+        .equ    FSEL4,12
+        .equ    FSEL3,9
+        .equ    FSEL2,6
+        .equ    FSEL1,3
+        .equ    FSEL0,0
+
+
+        .equ    GPSET0,0x1c
+        .equ    GPSET1,0x20
+
+        .equ    GPCLR0,0x28
+        .equ    GPCLR1,0x2c
+
+        .equ    _0,0
+        .equ    _1,1
+        .equ    _2,2
+        .equ    _3,3
+        .equ    _4,4
+        .equ    _5,5
+        .equ    _6,6
+        .equ    _7,7
+        .equ    _8,8
+        .equ    _9,9
+      .equ    _10,10
+        .equ    _11,11
+        .equ    _12,12
+        .equ    _13,13
+        .equ    _14,14
+        .equ    _15,15
+        .equ    _16,16
+        .equ    _17,17
+        .equ    _18,18
+        .equ    _19,19
+
+        .equ    _20,20
+        .equ    _21,20
+        .equ    _22,22
+        .equ    _23,23
+        .equ    _24,24
+        .equ    _25,25
+        .equ    _26,26
+        .equ    _27,27
+        .equ    _28,28
+        .equ    _29,29
+
+        .equ    _30,30
+        .equ    _31,31
+
+        .equ    _32,0
+        .equ    _33,1
+        .equ    _34,2
+        .equ    _35,3
+        .equ    _36,4
+        .equ    _37,5
+        .equ    _38,6
+        .equ    _39,7
+        .equ    _40,8
+        .equ    _41,9
+        .equ    _42,10
+        .equ    _43,11
+        .equ    _44,12
+        .equ    _45,13
+        .equ    _46,14
+        .equ    _47,15
+        .equ    _48,16
+        .equ    _49,17
+        .equ    _50,18
+        .equ    _51,19
+        .equ    _53,20
+        .equ    _53,21
